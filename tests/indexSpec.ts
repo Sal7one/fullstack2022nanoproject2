6ac4f128df64582b3a9f80345e9bcdd2adfd64b9
Saleh Alanazi <alanazi.saleh.77@gmail.com>
2023-02-09 19:07:31 +0300

    clean up

--- tests/indexSpec.ts
+++ tests/indexSpec.ts
@@ -1,6 +1,4 @@
-import supertest from "supertest";
 import {SpecReporter} from "jasmine-spec-reporter";
-import app from '../src/app';
 
 jasmine.getEnv().clearReporters();
 jasmine.getEnv().addReporter(
